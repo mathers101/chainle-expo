5ad0b2a1fba67a2c285241808e1784b0b012459d
TY Mathers <alexmathers19@gmail.com>
2025-07-11 21:32:45 -0700

    wasnt tagging the save data with todays date

--- lib/saveToLocalStorage.ts
+++ lib/saveToLocalStorage.ts
@@ -1,16 +1,20 @@
+import { SaveData } from "@/components/ChainContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getTodaysDate } from "./time";
 
-export async function saveToLocalStorage(data: any) {
+export async function saveToLocalStorage(data: SaveData) {
+  const date = getTodaysDate();
   try {
-    await AsyncStorage.setItem("chainle-save", JSON.stringify(data));
+    await AsyncStorage.setItem(`chainle-${date}`, JSON.stringify(data));
   } catch (e) {
     console.error(e);
   }
 }
 
-export async function getFromLocalStorage() {
+export async function getFromLocalStorage(): Promise<SaveData | null> {
+  const date = getTodaysDate();
   try {
-    const value = await AsyncStorage.getItem("chainle-save");
+    const value = await AsyncStorage.getItem(`chainle-${date}`);
     return value ? JSON.parse(value) : null;
   } catch (e) {
     console.error(e);
